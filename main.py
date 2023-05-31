9eea5a4a4dc8d7834cc7b355c2aac71b048a93df
Undefined1234 <74308010+Undefined1234@users.noreply.github.com>
2023-05-31 12:09:00 +0200

    Testing

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #this will be the main script for our drugdiscoverychallenge code. Please only merge working code here. 
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+from rdkit.Chem.Draw import IPythonConsole
+from rdkit.Chem import Draw
+IPythonConsole.ipython_useSVG=True
+
+m = Chem.MolFromSmiles('Cc1ccccc1')
+print(Descriptors.RingCount(m))
