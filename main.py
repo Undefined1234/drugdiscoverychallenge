8f791a762af1f7cbb237af878d39db85fd41a407
Matthieu den Otter <m.d.otter@student.tue.nl>
2023-05-31 11:04:58 +0200

    added test

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ from rdkit.Chem.Draw import IPythonConsole
 from rdkit.Chem import Draw
 IPythonConsole.ipython_useSVG=True
 
+
 m = Chem.MolFromSmiles("c1ccccc1-C-c1ccccc1")
 m
+
+import pandas as pd
+df = pd.read_csv('tested_molecules-1.csv',index_col=0)
+df.info()
+#Draw.MolsToGridImage([Chem.MolFromSmiles(s) for s in pu], molsPerRow=4, subImgSize=(200,200), legends=pu)
