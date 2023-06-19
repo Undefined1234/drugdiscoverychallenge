592ca09d99e7f51d7dc65c804922fb0551e70b74
Hielke van der Wal <hielke010@gmail.com>
2023-06-19 15:08:14 +0200

    resturcturing of the repo

--- main.py
+++
@@ -1,6 +0,0 @@
-#this will be the main script for our drugdiscoverychallenge code. Please only merge working code here. 
-
-import pandas as pd
-df = pd.read_csv('tested_molecules-1.csv')
-df.info()
-#Draw.MolsToGridImage([Chem.MolFromSmiles(s) for s in pu], molsPerRow=4, subImgSize=(200,200), legends=pu)
